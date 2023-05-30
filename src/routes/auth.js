2fcdea56254761809ea91bb11eceecc12eb24cb6
Quanghwi <huydqph24532@fpt.edu.vn>
2023-05-30 17:19:27 +0700

    feat:\ sign up - đăng kí - validate ref: BW-6

---
+++ src/routes/auth.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import { signUp } from '../controllers/auth';
+
+
+const routerAuth = express.Router();
+
+routerAuth.post('/signup', signUp)
+
+export default routerAuth
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import routerProduct from './product';
-
+import routerAuth from './auth'
 const router = express.Router();
 
 router.use('/products', routerProduct)
+router.use('/auth', routerAuth)
 
 export default router
