5dadc03521f8a64472c26427136c013750d239d2
Quanghwi <huydqph24532@fpt.edu.vn>
2023-05-30 16:19:08 +0700

    BW-1 feat:\ create product - thêm sản phẩm - validate - get detail

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,4 +1,6 @@
 import Product from '../models/product'
+import productSchema from '../validation/product';
+
 export const getAll = async (req, res) => {
     try {
         const products = await Product.find();
@@ -13,7 +15,50 @@ export const getAll = async (req, res) => {
         })
     } catch (error) {
         res.status(500).json({
-            message:"Lỗi server"
+            message: "Lỗi server"
+        })
+    }
+}
+export const getDetail = async (req, res) => {
+    try {
+        const products = await Product.findById(req.params.id);
+        if (!products) {
+            return res.status(400).json({
+                message: "Không tìm thấy sản phẩm"
+            })
+        }
+        return res.status(200).json({
+            message: " tìm sản phẩm thành công",
+            datas: products
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Lỗi server"
+        })
+    }
+}
+
+export const create = async (req, res) => {
+    try {
+        const { error } = productSchema.validate(req.body)
+        if (error) {
+            return res.status(400).json({
+                message: error.details[0].message
+            })
+        }
+        const product = await Product.create(req.body);
+        if (!product) {
+            return res.status(400).json({
+                message: "Không thêm được sản phẩm"
+            })
+        }
+        return res.status(200).json({
+            message: " Thêm sản phẩm thành công",
+            datas: product
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Lỗi server"
         })
     }
 }
