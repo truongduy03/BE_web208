17de81d2d855398f4eb46c3fa57349104d45e941
truongduy03 <truongduy97a@gmail.com>
2023-05-30 22:28:02 +0700

    BW-3 - updateSP

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -77,3 +77,24 @@ export const remove = async (req, res) => {
     }
   };
 
+
+  export const update = async (req, res) => {
+    try {
+      const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+      });
+      if (!product) {
+        return res.status(404).json({
+          message: "Không tìm thấy sản phẩm",
+        });
+      }
+      return res.status(200).json({
+        message: "Sản phẩm đã được cập nhật thành công",
+        data: product,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: error,
+      });
+    }
+  };
